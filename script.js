d833869148608971b15650b0dc37d57ba6df8161
mirela777 <70278105+mirela777@users.noreply.github.com>
2021-04-29 10:12:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
+var winner = Math.floor(Math.random() * 3) + 1;
 function guess(clicked_id){
-    var winner = Math.floor(Math.random() * 3) + 1;
     var message1 = "You win";
     var message2 = "You lose";
     var emoji = String.fromCodePoint(128546);
     var emoji2 = String.fromCodePoint(128516);
     if(clicked_id == winner){
         document.getElementById("text").innerHTML = message1 + emoji2; 
-      }
+        }
     else{
         document.getElementById("text").innerHTML = message2 + emoji;
         document.getElementById(clicked_id).disabled = true;
